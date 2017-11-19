79ee15bfe01838b6f8f2dc101d505cf1a858d3ef
Caio Filipe <caiofilipemr@gmail.com>
2017-11-19 02:56:37 -0200

    Adicionado luz, rotação e três sólidos.

--- main.cpp
+++ main.cpp
@@ -1,49 +1,118 @@
-#include <stdlib.h>
 #include <GL/glut.h>
 
-GLsizei WIDTH = 600, HEIGHT = 600;
-GLsizei MOUSEx, MOUSEy;
-GLfloat SIDE = 1;
+#define ROTATION_SENSIBILITY 5.0
 
-GLfloat RED[3] = {1,0,0};
-GLfloat GREEN[3] = {0,1,0};
-GLfloat BLUE[3] = {0,0,1};
-GLfloat WHITE[3] = {1,1,1};
-GLfloat BLACK[3] = {0,0,0};
-GLfloat YELLOW[3] = {1,1,0};
-GLfloat CYAN[3] = {0,1,1};
-GLfloat MAGENTA[3] = {1,0,1};
+GLfloat xRotation;
+GLfloat yRotation;
+GLfloat lightPosition[] = {-25.f, 0.f, 50.f, 1.f};
+int initialX;
+int initialY;
+int pressedButton;
 
-void display (void) {
-    glClearColor(1.0,1.0,1.0,0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-    glTranslatef(-0.5, -0.5, 0.0);
-    glFlush();
+void clearBuffers() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 }
 
-void drawPoint(int x, int y) {
-
+void drawCube() {
+    glPushMatrix();
+    glTranslatef(-10, 0, 0);
+    glRotatef(xRotation, 1, 0, 0);
+    glRotatef(yRotation, 0, 1, 0);
+    glColor3f(1.0, 0.0, 0.0);
+    glutSolidCube(5);
+    glPopMatrix();
 }
 
-void mouse(int btn, int state, int x, int y)
-{
-    if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN) {
+void drawSphere() {
+    glPushMatrix();
+    glTranslatef(0, 0, 0);
+    glRotatef(xRotation, 1, 0, 0);
+    glRotatef(yRotation, 0, 1, 0);
+    glColor3f(0.0, 1.0, 0.0);
+    glutSolidSphere(2.5, 40, 40);
+    glPopMatrix();
+}
 
+void drawCone() {
+    glPushMatrix();
+    glTranslatef(10, 0, 0);
+    glRotatef(xRotation, 1, 0, 0);
+    glRotatef(yRotation, 0, 1, 0);
+    glColor3f(0.0, 0.0, 1.0);
+    glutSolidCone(2.5, 2.5, 40, 40);
+    glPopMatrix();
+}
+
+void draw() {
+    clearBuffers();
+    drawCube();
+    drawSphere();
+    drawCone();
+    glutSwapBuffers();
+}
+
+void definePerspective() {
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(-15, 15, -15, 15, -15, 15);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void mouseClick(int button, int state, int x, int y) {
+    if (state == GLUT_DOWN) {
+        initialX = x;
+        initialY = y;
+        pressedButton = button;
+    } else {
+        pressedButton = -1;
     }
-    if(btn==GLUT_RIGHT_BUTTON && state==GLUT_DOWN) {
-        exit(1);
+}
+
+void mouseMove(int x, int y) {
+    if (pressedButton == GLUT_LEFT_BUTTON) {
+        int deltaX = initialX - x;
+        int deltaY = initialY - y;
+        xRotation = xRotation - deltaY / ROTATION_SENSIBILITY;
+        yRotation = yRotation - deltaX / ROTATION_SENSIBILITY;
+
+        initialX = x;
+        initialY = y;
     }
+    glutPostRedisplay();
+}
+
+void defineCallbacks() {
+    glutDisplayFunc(draw);
+    glutMouseFunc(mouseClick);
+    glutMotionFunc(mouseMove);
+}
+
+void defineWindowConfiguration() {
+    glutInitWindowSize(600, 600);
+    glutInitDisplayMode(GLUT_STENCIL | GLUT_DEPTH | GLUT_DOUBLE);
+    glutCreateWindow("CSG OpenGL");
+    glClearColor(1, 1, 1, 0);
+}
+
+void defineLightConfiguration() {
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+    glEnable(GL_COLOR_MATERIAL);
+}
+
+void init() {
+    defineWindowConfiguration();
+    definePerspective();
+    defineCallbacks();
+    defineLightConfiguration();
+    glutMainLoop();
 }
 
 int main (int argc, char **argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(WIDTH, HEIGHT);
-    glutInitWindowPosition(10, 10);
-    glutCreateWindow("New Window");
-    glutDisplayFunc(display);
-    glutMouseFunc(mouse);
-    glutMainLoop();
+    init();
     return 0;
 }
