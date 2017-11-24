538be3f5db2fa9eb9b27b1f26f5fe3c650390e34
caiofilipemr <caiofilipemr@gmail.com>
2017-11-24 18:26:28 -0200

    Conclusão, movimentos dos objetos criado. Utilizando teclas para mover e mudar o objeto que deve ser desenhado.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <GL/glut.h>
 
+struct Position {
+    float x, y, z;
+
+    Position(float x, float y, float z) : x(x), y(y), z(z) { }
+};
+
 GLfloat xRotation;
 GLfloat yRotation;
 GLfloat zRotation;
 GLfloat lightPosition[] = {-25.f, 0.f, 50.f, 1.f};
+
+Position positionFirst = Position(0, 0, 0);
+Position positionSecond = Position(0, 0, 0);
 
 void clearBuffers() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
@@ -17,12 +26,12 @@ void setViewAngle() {
     glRotatef(zRotation, 0, 0, 1);
 }
 
-void firstInsideSecond(void(*drawFirstObject)(void), void(*drawSecondObject)(void), GLenum face, GLenum test)
-{
+void drawUsingStencil(void(*drawFirstObject)(Position), Position p1, void(*drawSecondObject)(Position), Position p2,
+                      GLenum face, GLenum test) {
     glEnable(GL_DEPTH_TEST);
     glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
     glCullFace(face); /* controls which face of a to use*/
-    drawFirstObject(); /* draw a face of a into depth buffer */
+    drawFirstObject(p1); /* draw a face of a into depth buffer */
 
     /* use stencil plane to find parts of a in b */
     glDepthMask(GL_FALSE);
@@ -30,10 +39,10 @@ void firstInsideSecond(void(*drawFirstObject)(void), void(*drawSecondObject)(voi
     glStencilFunc(GL_ALWAYS, 0, 0);
     glStencilOp(GL_KEEP, GL_KEEP, GL_INCR);
     glCullFace(GL_BACK);
-    drawSecondObject(); /* increment the stencil where the front face of b is drawn */
+    drawSecondObject(p2); /* increment the stencil where the front face of b is drawn */
     glStencilOp(GL_KEEP, GL_KEEP, GL_DECR);
     glCullFace(GL_FRONT);
-    drawSecondObject(); /* decrement the stencil buffer where the back face of b is drawn */
+    drawSecondObject(p2); /* decrement the stencil buffer where the back face of b is drawn */
     glDepthMask(GL_TRUE);
     glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
 
@@ -41,69 +50,73 @@ void firstInsideSecond(void(*drawFirstObject)(void), void(*drawSecondObject)(voi
     glDisable(GL_DEPTH_TEST);
 
     glCullFace(face);
-    drawFirstObject(); /* draw the part of a that's in b */
+    drawFirstObject(p1); /* draw the part of a that's in b */
     glDisable(GL_STENCIL_TEST);
 }
 
-void fixDepth(void(*drawObject)(void))
-{
+void fixDepth(void(*drawObject)(Position), Position p) {
     glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_ALWAYS);
-    drawObject();
+    drawObject(p);
     glDepthFunc(GL_LESS);
     glDisable(GL_DEPTH_TEST);
     glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
 }
 
-void drawCube() {
+void drawCube(Position p) {
     glPushMatrix();
-    glTranslatef(0, 0, 0);
+    glTranslatef(p.x, p.y, p.z);
     glColor3f(1.0, 0.0, 0.0);
     glutSolidCube(5);
     glPopMatrix();
 }
 
-void drawSphere() {
+void drawSphere(Position p) {
     glPushMatrix();
-    glTranslatef(2.5f, 2.5, 2.5);
+    glTranslatef(p.x, p.y, p.z);
     glColor3f(0.0, 1.0, 0.0);
-    glutSolidSphere(2.5, 40, 40);
+    glutSolidSphere(3, 40, 40);
     glPopMatrix();
 }
 
-void drawCone() {
+void drawCone(Position p) {
     glPushMatrix();
-    glRotatef(270, 1, 0, 0);
-    glTranslatef(0, 0, -3);
+    glTranslatef(p.x, p.y, p.z);
     glColor3f(0.0, 0.0, 1.0);
     glutSolidCone(2.5, 8, 40, 40);
     glPopMatrix();
 }
 
-void _union(void(*drawFirstObject)(void), void(*drawSecondObject)(void)) {
+
+void _union(void(*drawFirstObject)(Position), Position p1, void(*drawSecondObject)(Position), Position p2) {
     glEnable(GL_DEPTH_TEST);
-    drawFirstObject();
-    drawSecondObject();
+    drawFirstObject(p1);
+    drawSecondObject(p2);
     glDisable(GL_DEPTH_TEST);
 }
 
-void intersection(void(*drawFirstObject)(void), void(*drawSecondObject)(void)) {
-    firstInsideSecond(drawFirstObject, drawSecondObject, GL_BACK, GL_NOTEQUAL);
-    fixDepth(drawSecondObject);
-    firstInsideSecond(drawSecondObject, drawFirstObject, GL_BACK, GL_NOTEQUAL);
+void intersection(void(*drawFirstObject)(Position), Position p1, void(*drawSecondObject)(Position), Position p2) {
+    drawUsingStencil(drawFirstObject, p1, drawSecondObject, p2, GL_BACK, GL_NOTEQUAL);
+    fixDepth(drawSecondObject, p2);
+    drawUsingStencil(drawSecondObject, p1, drawFirstObject, p2, GL_BACK, GL_NOTEQUAL);
 }
 
-void subtraction(void(*drawFirstObject)(void), void(*drawSecondObject)(void)) {
-    firstInsideSecond(drawFirstObject, drawSecondObject, GL_FRONT, GL_NOTEQUAL);
-    fixDepth(drawSecondObject);
-    firstInsideSecond(drawSecondObject, drawFirstObject, GL_BACK, GL_EQUAL);
+void subtraction(void(*drawFirstObject)(Position), Position p1, void(*drawSecondObject)(Position), Position p2) {
+    drawUsingStencil(drawFirstObject, p1, drawSecondObject, p2, GL_FRONT, GL_NOTEQUAL);
+    fixDepth(drawSecondObject, p1);
+    drawUsingStencil(drawSecondObject, p1, drawFirstObject, p2, GL_BACK, GL_EQUAL);
 }
+
+void (*fnDrawFirstObject)(Position) = drawSphere;
+void (*fnDrawSecondObject)(Position) = drawCone;
+
+void (*fnBooleanOp)(void(*drawFirstObject)(Position), Position p1, void(*drawSecondObject)(Position), Position p2) = _union;
 
 void draw() {
     clearBuffers();
     setViewAngle();
-    _union(drawCube, drawCone);
+    fnBooleanOp(fnDrawFirstObject, positionFirst, fnDrawSecondObject, positionSecond);
     glutSwapBuffers();
 }
 
@@ -126,6 +139,50 @@ void keyboardKeys(unsigned char key, int, int) {
             break;
         case '.':
             zRotation -= 1;
+        case 'z':
+            fnDrawFirstObject = drawSphere;
+            break;
+        case 'x':
+            fnDrawSecondObject = drawSphere;
+            break;
+        case 'c':
+            fnDrawFirstObject = drawCone;
+            break;
+        case 'v':
+            fnDrawSecondObject = drawCone;
+            break;
+        case 'b':
+            fnDrawFirstObject = drawCube;
+            break;
+        case 'n':
+            fnDrawSecondObject = drawCube;
+            break;
+        case 'u':
+            fnBooleanOp = _union;
+            break;
+        case 'i':
+            fnBooleanOp = intersection;
+            break;
+        case 'o':
+            fnBooleanOp = subtraction;
+            break;
+        case 'q':
+            positionFirst.x = positionFirst.x + 0.1f;
+            break;
+        case 'a':
+            positionFirst.x = positionFirst.x - 0.1f;
+            break;
+        case 'w':
+            positionFirst.y = positionFirst.y + 0.1f;
+            break;
+        case 's':
+            positionFirst.y = positionFirst.y - 0.1f;
+            break;
+        case 'e':
+            positionFirst.z = positionFirst.z + 0.1f;
+            break;
+        case 'd':
+            positionFirst.z = positionFirst.z - 0.1f;
             break;
         default:
             break;
